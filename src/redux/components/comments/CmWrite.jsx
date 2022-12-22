e81fd33299a98783c7a1e6a148c649cb0734ba61
tyoung <taeyoung917@gmail.com>
2022-12-23 01:31:25 +0900

    댓글,이름,비밀번호, 추가버튼 생성

---
+++ src/redux/components/comments/CmWrite.jsx
@@ -0,0 +1,162 @@
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import Button from '../button/Button';
+import { v4 as uuidv4 } from 'uuid';
+
+function CmWrite({ setCommentList, commentList }) {
+  const [comment, setComment] = useState('');
+  const [userId, setUserId] = useState('');
+  const [userPw, setUserPw] = useState('');
+  const [inputTxt, setInpuTxt] = useState({
+    commentTxt: '',
+    userIdTxt: '',
+    userPwTxt: '',
+  });
+
+  const { commentTxt, userIdTxt, userPwTxt } = inputTxt;
+
+  //댓글 입력창
+  const commentOnChange = (e) => {
+    setComment(e.target.value);
+  };
+  //유저이름 입력창
+  const userIdOnChange = (e) => {
+    setUserId(e.target.value);
+  };
+  //비밀번호 입력창
+  const userPwOnChange = (e) => {
+    setUserPw(e.target.value);
+  };
+
+  //댓글 추가버튼
+  const commentOnSubmit = (e) => {
+    e.preventDefault();
+    //현재시간 객체로 받음.
+    let today = new Date();
+
+    const newComment = {
+      id: uuidv4(),
+      comment,
+      userId,
+      userPw,
+      date: today.toLocaleDateString(),
+    };
+
+    //form 유효검사
+    if (!comment || !userId || !userPw) {
+      setInpuTxt({
+        ...inputTxt,
+        commentTxt: '댓글 을 입력해주세요.',
+        userIdTxt: '이름 을 입력해주세요.',
+        userPwTxt: '비밀번호 를 입력해주세요.',
+      });
+      return;
+    }
+
+    setCommentList([...commentList, newComment]);
+    setComment('');
+    setUserId('');
+    setUserPw('');
+  };
+
+  return (
+    <>
+      <StyleForm onSubmit={commentOnSubmit}>
+        <div className="comment-wrap">
+          <input
+            maxLength="40"
+            type="text"
+            value={comment}
+            onChange={commentOnChange}
+            placeholder="댓글을 입력해주세요."
+          />
+          {/* 코멘트 입력했을때  */}
+          <StyleInputErro>{comment ? '' : commentTxt}</StyleInputErro>
+        </div>
+
+        <StyleFlex>
+          <div className="user-infor">
+            <div>
+              <input
+                maxLength="5"
+                type="text"
+                value={userId}
+                onChange={userIdOnChange}
+                placeholder="이름을 입력해주세요"
+              />
+              <StyleInputErro>{userId ? '' : userIdTxt}</StyleInputErro>
+            </div>
+
+            <div>
+              <input
+                maxLength="10"
+                type="text"
+                value={userPw}
+                onChange={userPwOnChange}
+                placeholder="비밀번호를 입력 해 주세요"
+              />
+              <StyleInputErro>{userPw ? '' : userPwTxt}</StyleInputErro>
+            </div>
+          </div>
+        </StyleFlex>
+        <Button backgroundColor="#0a0a0a" radius="100">
+          댓글추가
+        </Button>
+      </StyleForm>
+    </>
+  );
+}
+
+export default CmWrite;
+
+const StyleForm = styled.form`
+  display: flex;
+  justify-content: space-between;
+  align-items: flex-start;
+  padding: 16px 24px;
+
+  .comment-wrap {
+    flex: 1;
+
+    > input {
+      width: 100%;
+      height: 34px;
+      border: none;
+      border-radius: 50px;
+      padding: 0 8px;
+
+      :focus {
+        outline: none;
+      }
+    }
+  }
+`;
+
+const StyleFlex = styled.div`
+  display: flex;
+  flex: 1.5;
+  justify-content: flex-end;
+
+  .user-infor {
+    display: flex;
+
+    > div {
+      > input {
+        height: 34px;
+        padding: 0 8px;
+        margin-right: 8px;
+        border: none;
+        border-radius: 50px;
+
+        :focus {
+          outline: none;
+        }
+      }
+    }
+  }
+`;
+
+const StyleInputErro = styled.p`
+  color: red;
+  font-size: 13px;
+`;
