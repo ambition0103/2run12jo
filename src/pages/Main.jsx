785f7e041c76f88680d7a873dc983f685f73c901
tyoung <taeyoung917@gmail.com>
2022-12-22 17:11:22 +0900

    버튼컴포넌트 사용법

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,11 +1,24 @@
-import React from 'react'
+import React from 'react';
+import Button from '../redux/components/button/Button';
 
 function Main() {
   return (
     <div>
       메인메인
+      {/* 버튼1 */}
+      <Button backgroundColor="#2F80ED"  radius="4">
+        버튼컴포넌트
+      </Button>
+      {/* 버튼2 */}
+      <Button backgroundColor="#0a0a0a" radius="100">
+        버튼컴포넌트
+      </Button>
+      {/* 버튼3 */}
+      <Button backgroundColor="#8d8d8d" radius="4">
+        취소
+      </Button>
     </div>
-  )
+  );
 }
 
-export default Main
+export default Main;
