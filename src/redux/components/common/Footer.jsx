f2b9a10ae0be88fe4ba321f6429f594a499b6f85
tyoung <taeyoung917@gmail.com>
2022-12-22 16:25:39 +0900

    푸터수정

--- src/redux/components/common/Footer.jsx
+++ src/redux/components/common/Footer.jsx
@@ -1,11 +1,7 @@
-import React from 'react'
+import React from 'react';
 
 function Footer() {
-  return (
-    <div>
-      푸--터
-    </div>
-  )
+  return <div>푸--터22</div>;
 }
 
-export default Footer
+export default Footer;
