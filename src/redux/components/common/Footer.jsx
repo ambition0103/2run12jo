0817d9f887833767ebb65458f5c9bd967ea28800
Lee Jae Sung <110893338+ambition0103@users.noreply.github.com>
2022-12-26 21:20:15 +0900

    Delete src directory

--- src/redux/components/common/Footer.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-function Footer() {
-  return <div>푸--터22</div>;
-}
-
-export default Footer;
--- src/redux/components/common/Header.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-function Header() {
-  return <div>헤--더ㅇㅇ</div>;
-}
-
-export default Header;
