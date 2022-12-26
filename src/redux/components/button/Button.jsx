5d2f76d086411a5f62ab0da392b8e451cadbcb1a
tyoung <taeyoung917@gmail.com>
2022-12-26 20:11:19 +0900

    css 패딩값 추가

--- src/redux/components/button/Button.jsx
+++ src/redux/components/button/Button.jsx
@@ -1,14 +1,21 @@
+import React from 'react';
+import styled from 'styled-components';
 
-
-import React from "react";
-import styled from "styled-components";
-
-function Button({ children, backgroundColor, radius, ClickHandler }) {
+function Button({
+  children,
+  backgroundColor,
+  radius,
+  ClickHandler,
+  sidePadding,
+  upDownPadding,
+}) {
   return (
     <StyleButton
       backgroundColor={backgroundColor}
       radius={radius}
       onClick={ClickHandler}
+      upDownPadding={upDownPadding}
+      sidePadding={sidePadding}
     >
       {children}
     </StyleButton>
@@ -18,10 +25,11 @@ function Button({ children, backgroundColor, radius, ClickHandler }) {
 export default Button;
 
 const StyleButton = styled.button`
-  padding: 8px 16px;
+  padding: ${(props) => props.upDownPadding || '8'}px
+    ${(props) => props.sidePadding || '16'}px;
   color: #fff;
   font-weight: 700;
-  background: ${(props) => props.backgroundColor};
+  background: ${(props) => props.backgroundColor || '#3187F1'};
   border: 0;
   cursor: pointer;
   border-radius: ${(props) => props.radius}px;
