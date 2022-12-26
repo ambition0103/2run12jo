eae0025e84038158c9028bf25f79322b428f19a5
tyoung <taeyoung917@gmail.com>
2022-12-26 20:09:38 +0900

    todoList get, post

--- src/redux/components/TodoList/Todo.jsx
+++ src/redux/components/TodoList/Todo.jsx
@@ -1,27 +1,54 @@
-import React from "react";
-import { useNavigate } from "react-router-dom";
-import styled from "styled-components";
-import Detail from "../../../pages/Detail";
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import styled from 'styled-components';
+import Button from '../button/Button';
+import { __deleteTodos } from '../../modules/todosSlice';
+import { useDispatch } from 'react-redux';
 
-const Todo = ({ item: t }) => {
+const Todo = ({ item }) => {
+  const dispatch = useDispatch();
+
+  //링크이동
   const navigate = useNavigate();
   const goToDetail = () => {
-    navigate(`/${t.id}`);
+    navigate(`/${item.id}`);
+  };
+
+  //일정삭제
+  const todoListDeleteButton = () => {
+    const deleteCommnet = window.prompt(
+      '삭제를 위해서 비밀번호를 입력 해 주세요.',
+      '비밀번호를 입력해주세요.'
+    );
+
+    //비밀번호가 같을 때
+    if (deleteCommnet === item.userPw) {
+      window.confirm('정말 삭제하겠습니까?');
+      dispatch(__deleteTodos(item.id));
+    } else {
+      window.confirm('비밀번호가 틀립니다. 다시 입력 해 주세요.');
+    }
   };
 
   return (
-    <div key={t.id} onClick={goToDetail}>
-      <h4>제목: {t.title}</h4>
-      <p>내용: {t.content}</p>
-    </div>
+    <StyledTodoBox key={item.id}>
+      <div onClick={goToDetail}>
+        <h4>제목: {item.title}</h4>
+        <p>id: {item.id}</p>
+        <p>내용: {item.content}</p>
+        <p>schedule: {item.schedule}</p>
+        <p>이름: {item.userId}</p>
+        <p>비밀번호: {item.userPw}</p>
+        <p>마감일: {item.doneDate}</p>
+        <p>시작일: {item.startDate}</p>
+      </div>
+      <Button ClickHandler={todoListDeleteButton}>일정삭제</Button>
+    </StyledTodoBox>
   );
 };
 
 export default Todo;
 
 const StyledTodoBox = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
   border: 1px solid black;
 `;
--- src/redux/components/TodoList/TodoList.jsx
+++ src/redux/components/TodoList/TodoList.jsx
@@ -1,23 +1,29 @@
-import React, { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { __getTodos, __postTodos } from "../../modules/todosSlice";
-import Todo from "./Todo";
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { __getTodos, __postTodos } from '../../modules/todosSlice';
+import Todo from './Todo';
 
 const TodoList = () => {
   const dispatch = useDispatch();
-  const { isLoading, error, todos, isAdded } = useSelector(
-    (state) => state.allTodos
-  );
+  const { isLoading, error, todos } = useSelector((state) => state.allTodos);
 
   useEffect(() => {
     // post요청이 끝나면 isAdded 값이 변경되고 __getTodos()가 실행된다.
     // 그러면 AddScheduleInput 컴포넌트에서 새로운 todo를 입력 시 todo 화면이 리렌더링 되고 새로운 todo가 바로 보인다.
     dispatch(__getTodos());
-  }, [isAdded]);
+  }, [dispatch]);
 
-  // const todos = useSelector((state) => state.todosSlice.todos);
+  console.log(isLoading, error);
 
   //. todos의 데이터가 없으면 사용자 편의를 위해 아래 메시지를 띄운다.
+  if (isLoading === 0) {
+    return (
+      <div>
+        <h3>로딩중 입니다...</h3>
+        <p>잠시만 기다려 주세요.</p>
+      </div>
+    );
+  }
   if (todos.length === 0) {
     return (
       <div>
